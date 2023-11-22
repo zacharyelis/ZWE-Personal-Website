e5b072ded1a752569737458f92bab2268f9fbe50
Zachary Elis <zacharyelis13@gmail.com>
2023-11-21 21:00:43 -0500

    Updated dependencies

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -1,6 +1,6 @@
 import "./projects.css";
 import WebsiteScreenshot from "./WebsiteScreenshot.png";
-import GameScreenshot from "./GameScreenshot.png";
+import { FaGithub } from "react-icons/fa";
 
 export default function projects() {
     return(
@@ -13,7 +13,10 @@ export default function projects() {
                         <div className="project-title">Personal Website</div>
                         <div className="project-description">This website! I created it to share some personal 
                         information, display my experience, and show off some web development skills. I used 
-                        React.js, Bootstrap, CSS, HTML, and JavaScript.</div>
+                            React.js, Bootstrap, CSS, HTML, and JavaScript.</div>
+                        <a href="https://github.com/zacharyelis/zwe-personal-website" target="_blank" rel="noopener noreferrer">
+                            <FaGithub size={30} />
+                        </a>
                     </div>
                 </div>
            </div>
